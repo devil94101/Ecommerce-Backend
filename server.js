4bcdba0d0b0f5cff809cd3f7ef4aeb488c32494d
devil94101 <36105759+devil94101@users.noreply.github.com>
2020-10-06 11:55:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.use(express.json())
 app.use(cors())
 app.use('/user',user)
 app.use('/product',product)
+app.use(function (req, res, next) {
+
+   res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 const PORT=process.env.PORT||5000;
 const uri = process.env.URI;
 mongoose.connect(uri, {
